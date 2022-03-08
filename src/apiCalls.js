2e670adf9ef9008038486f4c2e7bd6b865f7df58
Oscar Cortez <oacortez98@gmail.com>
2022-03-08 10:33:54 -0700

    Create post request to add new user and ingredients

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,17 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const postOrders = (order) => {
+  return fetch('http://localhost:3001/api/v1/orders', {
+    method: 'POST', 
+    body: JSON.stringify({
+      name: order.name,
+      ingredients: order.ingredients
+    }), 
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+}
