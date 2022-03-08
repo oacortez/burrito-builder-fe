4e298cf4025dc7bed8c10719a7b7f480f4a7941d
Oscar Cortez <oacortez98@gmail.com>
2022-03-08 10:34:32 -0700

    Add key value to return statements on order component

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './Orders.css';
 
 const Orders = props => {
+
   const orderEls = props.orders.map(order => {
     return (
-      <div className="order">
+      <div key={order.id} className="order" >
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
-          {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
+          {order.ingredients.map((ingredient, index) => {
+            return <li key={index}>{ingredient}</li>
           })}
         </ul>
       </div>
